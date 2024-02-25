5d93e07c313e3b0325edc255331da2b3b1ab25c5
vyro <vyro@vyros-MacBook-Pro.local>
2024-02-25 13:34:14 -0800

    add propert screen

--- home/src/main/java/com/example/home/addnewproperty/ui/Components/ui/screens/AddPropertyScreen1.kt
+++ home/src/main/java/com/example/home/addnewproperty/ui/Components/ui/screens/AddPropertyScreen1.kt
@@ -1,2 +1,35 @@
 package com.example.home.addnewproperty.ui.Components.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+
+@Composable
+fun AddNewScreen1()
+{
+
+    Column (    modifier= Modifier.padding(20.dp)
+    ){
+
+        Icon(
+            imageVector = Icons.Filled.ArrowBack,
+            contentDescription ="Back Arrow",
+            tint=MaterialTheme.colorScheme.outline,
+            modifier = Modifier.align( Alignment.Start)
+        )
+
+
+
+    }
+
+}
