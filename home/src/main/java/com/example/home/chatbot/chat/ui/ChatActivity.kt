b93e6ef361cd991b99c1922c5ebffc423c071f93
MueedHassan <mhassan.bscs20seecs@seecs.edu.pk>
2024-04-18 12:35:46 +0500

    adding all

--- home/src/main/java/com/example/home/chatbot/chat/ui/ChatActivity.kt
+++ home/src/main/java/com/example/home/chatbot/chat/ui/ChatActivity.kt
@@ -1,2 +1,28 @@
 package com.example.home.chatbot.chat.ui
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import org.koin.androidx.viewmodel.ext.android.stateViewModel
+
+class ChatActivity : ComponentActivity() {
+
+    private val viewModel: ChatViewModel by stateViewModel(
+        state = { intent?.extras ?: Bundle() }
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ChatGptBotAppTheme {
+                ChatScreen(
+                    uiHandlers = ChatScreenUiHandlers(
+                        onSendMessage = viewModel::sendMessage,
+                        onResendMessage = viewModel::resendMessage
+                    ),
+                    conversation = viewModel.conversation,
+                    isSendingMessage = viewModel.isSendingMessage
+                )
+            }
+        }
+    }
+}
--- home/src/main/java/com/example/home/chatbot/di/NetworkModule.kt
+++ home/src/main/java/com/example/home/chatbot/di/NetworkModule.kt
@@ -1,2 +1,18 @@
 package com.example.home.chatbot.di
 
+import com.aallam.openai.client.OpenAI
+import com.aallam.openai.client.OpenAIConfig
+import org.koin.dsl.module
+
+val networkModule = module {
+    single { provideOpenAI() }
+}
+
+fun provideOpenAI() : OpenAI {
+    val config = OpenAIConfig(
+        token = BuildConfig.OPEN_AI_API_KEY
+    )
+
+    return OpenAI(config)
+
+}
